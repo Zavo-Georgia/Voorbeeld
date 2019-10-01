fd64120f5a639024ddf4d7d2217c55e1438f3ab1
Zavo-Georgia <z6249@zavo.be>
2019-10-01 09:04:27 +0200

    JS dobeelsteen afbeelding

--- js/dobbelsteen.js
+++ js/dobbelsteen.js
@@ -3,10 +3,11 @@ var uitvoerDobbelsteen = document.getElementById("uitvoerDobbelsteen");
 
 btnDobbelsteen.addEventListener("click", function() {
     let getal = getRandomInt(1, 6);
-    console.log(Math.round(Math.random() * 10));
+    
+    uitvoerDobbelsteen.innerHTML = getal;
 })
 
 
 function getRandomInt(min, max) {
-    return Math.round(Math.random() * (max - min+1)) +min
+    return Math.round(Math.random() * (max - min) + min);
 }
