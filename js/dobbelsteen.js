67ba83100e8c4ce9d96cdee9b56836020d3fdd26
Zavo-Georgia <z6249@zavo.be>
2019-10-01 10:07:49 +0200

    Update dobbelsteen.js

--- js/dobbelsteen.js
+++ js/dobbelsteen.js
@@ -3,11 +3,53 @@ var uitvoerDobbelsteen = document.getElementById("uitvoerDobbelsteen");
 
 btnDobbelsteen.addEventListener("click", function() {
     let getal = getRandomInt(1, 6);
+    let image = document.createElement("img");
     
-    uitvoerDobbelsteen.innerHTML = getal;
+   /* if (getal === 1){
+        image.src = "../img/1.gif";
+    }    
+    else if (getal === 2){
+        image.src = "../img/2.gif";
+    }    
+    else if (getal === 3){
+        image.src = "../img/3.gif";
+    }    
+    else if (getal === 4){
+        image.src = "../img/4.gif";
+    }    
+    else if (getal === 5){
+        image.src = "../img/5.gif";
+    }    
+    else if (getal === 6){
+        image.src = "../img/6.gif";
+    }*/
+    
+    switch(getal){
+        case 1:
+            image.src = "../img/1.gif";
+            break;        
+        case 2:
+            image.src = "../img/2.gif";
+            break;        
+        case 3:
+            image.src = "../img/3.gif";
+            break;        
+        case 4:
+            image.src = "../img/4.gif";
+            break;        
+        case 5:
+            image.src = "../img/5.gif";
+            break;        
+        case 6:
+            image.src = "../img/6.gif";
+            break;
+    }
+    
+    uitvoerDobbelsteen.innerHTML = "";
+    uitvoerDobbelsteen.append(image);
 })
 
-
+//functie om een willekeurig getal te verkijgen tussen min en max. min en max zijn parameters die worden meegegeven wanneer de functie wordt aangeroepen
 function getRandomInt(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
